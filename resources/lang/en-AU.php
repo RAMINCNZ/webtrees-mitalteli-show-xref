a90f274755fda8e73fad587e85331ceeffd64065
RAMINCNZ <RAMINCNZ@users.noreply.github.com>
2025-06-26 10:43:48 +1200

    Enable/Disable Gender Background Colours

---
+++ resources/lang/en-AU.php
@@ -0,0 +1,9 @@
+<?php
+
+    return [
+
+    "Enable/Disable CSS Gender Background Colors" => 'Enable/Disable CSS Gender Background Colours', // resources/views/settings.phtml
+    "CSS gender background colors are disabled." => 'CSS Gender Background Colours are Disabled.', // resources/views/sidebar.phtml - Line 33
+
+    ];
+?>
