fb4627992204d3e12feeb3aeaf8d30fb87d82457
TheDutchJewel <46184729+TheDutchJewel@users.noreply.github.com>
2025-08-18 21:52:33 +0200

    Added Dutch language file

---
+++ resources/lang/nl.php
@@ -0,0 +1,27 @@
+<?php
+
+    return [
+
+    "XREF and UID values module." => 'Module voor opvragen van XREF- en UID-waarden.', // modules_v4/mitalteli-show-xref/ShowXrefModule.php - function description() - Line 98
+    "A sidebar to show XREF and UID values." => 'Een zijbalk om XREF- en UID-waarden weer te geven.', // modules_v4/mitalteli-show-xref/ShowXrefModule.php\nLine 29
+    'The preferences for the module “%s” have been updated.' => 'De voorkeuren voor de module “%s” zijn bijgewerkt.', // modules_v4/mitalteli-show-xref/ShowXrefModule.php
+    #"Control panel" => 'Panel de control', // resources/views/settings.phtml
+    "back to the last individual page" => 'terug naar de pagina van de laatste persoon', // resources/views/settings.phtml
+    "Expand this module's sidebar content" => 'De inhoud van de zijbalk van deze module uitvouwen', // resources/views/settings.phtml
+    "If checked, this module's sidebar content will expand while the sidebar Family navigator will collapse if the user has the role of editor or higher." => 'Als dit vinkje is aangevinkt, wordt de inhoud van de zijbalk van deze module uitgevouwen, terwijl de gezinsnavigator in de zijbalk wordt ingeklapt als de gebruiker de rol van redacteur of hoger heeft.', // resources/views/settings.phtml
+    "Include UID values" => 'UID-waarden opnemen', // resources/views/settings.phtml
+    'Sidebar order' => 'Volgorde van de zijbalk', // resources/views/settings.phtml
+    "save" => 'opslaan', // resources/views/settings.phtml
+    "Enable/Disable CSS Gender Background Colors" => 'CSS-achtergrondkleuren voor geslacht in-/uitschakelen', // resources/views/settings.phtml
+    "Gender background colors are disabled." => 'Achtergrondkleuren voor geslacht zijn uitgeschakeld.', // resources/views/sidebar.phtml - Line 33
+    "XREF and UID information" => 'XREF- en UID-informatie', // resources/views/sidebar.phtml - Line 13 - title of sidebar
+    "XREF information" => 'XREF-informatie', // resources/views/sidebar-header.phtml - Line 13 - title of sidebar
+    #'Private' => 'Private', // resources/views/sidebar-xref.phtml
+    #'Parents' => 'Parents', // resources/views/sidebar-xref.phtml
+    #'Family' => 'Family', // resources/views/sidebar-xref.phtml
+    "UID information is disabled." => 'UID-informatie is uitgeschakeld.', // resources/views/sidebar-header.phtml - Line 13 - title of sidebar
+    #'Individual' => 'Individual', // resources/views/sidebar.phtml
+    #'Step children' => 'Step children', // resources/views/sidebar.phtml
+
+    ];
+?>
